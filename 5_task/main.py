9398e9d1d63b826339c012c7d7f64eab46d0120c
Ivan <113239177+paiksonn@users.noreply.github.com>
2022-12-17 16:24:28 +0300

    Update main.py

--- 5_task/main.py
+++ 5_task/main.py
@@ -1,30 +1,28 @@
 import json
 from random import randint
 
+class SomeNumber:
+    def __init__(self, month, year):
+        self.month = month
+        self.year = year
 
-class WorkHours:
 
-    def work_hours(self, month, year):
+    def get_number(self):
         return randint(20, 23)
 
-class Income_Per_Hour:
 
-    def income_per_hour(salary, month, year):
-        hour_income = salary/WorkHours().work_hours(month,year)
-        return round(hour_income, 2)
+class CalculateHourSalary:
+    def __init__(self, input_name_file='input.json', output_name_file='output.json'):
+        self.input_name_file = input_name_file
+        self.output_name_file = output_name_file
 
+    def get_hour_income(self):
+        with open(self.input_name_file, "r") as input_file:
+            data = json.load(input_file)
+            days_in_month = SomeNumber(data['month']), data['year'].get_number()
+            data['hour_income'] = round(data['salary'] / (days_in_month * 8), 2)
+        return data
 
-def main():
-
-    with open('input.json', 'r') as inputt:
-        data = json.load(inputt)
-        salary, month, year = data.get("salary"), data.get('month'), data.get('year')
-        PerHourSalary = Income_Per_Hour.income_per_hour(salary, month, year)
-
-    with open('output.json', 'w') as outputt:
-        data["hour_income"] = perHourSalary
-        outputt.write(json.dumps(data, ensure_ascii=False))
-
-
-if __name__ == '__main__':
-    main()
+    def write(self, data_json):
+        with open(self.output_name_file, 'a+') as result:
+            json.dump(data_json, result)
